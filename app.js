13e2f34bc6ceed2aa8469af656506b01b1465713
Joao Sallaberry <joaovictorhs@gmail.com>
2015-04-10 01:10:05 -0300

    reorganized folders

--- app.js
+++ app.js
@@ -1,33 +1,60 @@
-var app = angular.module('meanApp', []);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.controller('MainCtrl', ['$scope', 
-	function($scope) {
-		$scope.test = 'lal'
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-		// post list
-		$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-		];
+var app = express();
 
-		// Add post
-		$scope.addPost = function(){
-			if(!$scope.title || $scope.title === '') { return; }
-			$scope.posts.push({
-				title: $scope.title,
-				link: $scope.link,
-				upvotes: 0
-			});
-			$scope.title = '';
-			$scope.link = '';
-		};
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-		// Upvote
-		$scope.incrementUpvotes = function(post) {
-			post.upvotes += 1;
-		};
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-	}]);
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
